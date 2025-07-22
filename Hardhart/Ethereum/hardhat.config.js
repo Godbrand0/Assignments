b38544f2e79cd625102c5b514df3cdbd48c574cd
Thompson <140930314+Godbrand0@users.noreply.github.com>
2025-07-22 23:19:25 +0100

    Deploy and verify Storage contract on Sepolia using Hardhat and foundry

---
+++ Hardhart/Ethereum/hardhat.config.js
@@ -0,0 +1,30 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+const {PRIVATE_KEY, ETHERSCAN_KEY, ETHSEPHOLIA_URL_KEY, CORESCAN_KEY, COREDAO_URL_KEY} = process.env;
+
+module.exports = {
+  solidity: "0.8.30",
+  networks: {
+    // sepolia: {
+    //   url: ETHSEPHOLIA_URL_KEY,
+    //   accounts: [`0x${PRIVATE_KEY}`],
+    // },
+    core: {
+      url: COREDAO_URL_KEY,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+  // etherscan: {
+  //   apiKey: {
+  //     sepolia: ETHERSCAN_KEY,
+  //   },
+  // },
+  corescan: {
+    apiKey: CORESCAN_KEY,
+  },
+};
+
+
